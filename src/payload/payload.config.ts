49ce5e0f3e3e1aec08c2bcf75b3a9cf5c403ef98
dundas <git@daviddundas.com>
2024-05-13 11:59:59 -0500

    Initial commit

---
+++ src/payload/payload.config.ts
@@ -0,0 +1,134 @@
+import { webpackBundler } from '@payloadcms/bundler-webpack'
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import { payloadCloud } from '@payloadcms/plugin-cloud'
+import nestedDocs from '@payloadcms/plugin-nested-docs'
+import redirects from '@payloadcms/plugin-redirects'
+import seo from '@payloadcms/plugin-seo'
+import type { GenerateTitle } from '@payloadcms/plugin-seo/types'
+import { slateEditor } from '@payloadcms/richtext-slate'
+import dotenv from 'dotenv'
+import path from 'path'
+import { buildConfig } from 'payload/config'
+
+import Categories from './collections/Categories'
+import Comments from './collections/Comments'
+import { Media } from './collections/Media'
+import { Pages } from './collections/Pages'
+import { Posts } from './collections/Posts'
+import { Projects } from './collections/Projects'
+import Users from './collections/Users'
+import BeforeDashboard from './components/BeforeDashboard'
+import BeforeLogin from './components/BeforeLogin'
+import { seed } from './endpoints/seed'
+import { Footer } from './globals/Footer'
+import { Header } from './globals/Header'
+import { Settings } from './globals/Settings'
+import { s3Adapter } from '@payloadcms/plugin-cloud-storage/s3';
+import { cloudStorage } from '@payloadcms/plugin-cloud-storage';
+
+const storageAdapter = s3Adapter({
+  config: {
+    endpoint: process.env.S3_ENDPOINT,
+    region: process.env.S3_REGION,
+    credentials: {
+      accessKeyId: process.env.S3_ACCESS_KEY,
+      secretAccessKey: process.env.S3_SECRET_KEY,
+    }
+  },
+  bucket: process.env.S3_BUCKET_NAME,
+})
+
+const generateTitle: GenerateTitle = () => {
+  return 'My Website'
+}
+
+dotenv.config({
+  path: path.resolve(__dirname, '../../.env'),
+})
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+    bundler: webpackBundler(),
+    components: {
+      // The `BeforeLogin` component renders a message that you see while logging into your admin panel.
+      // Feel free to delete this at any time. Simply remove the line below and the import `BeforeLogin` statement on line 15.
+      beforeLogin: [BeforeLogin],
+      // The `BeforeDashboard` component renders the 'welcome' block that you see after logging into your admin panel.
+      // Feel free to delete this at any time. Simply remove the line below and the import `BeforeDashboard` statement on line 15.
+      beforeDashboard: [BeforeDashboard],
+    },
+    webpack: config => ({
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          dotenv: path.resolve(__dirname, './dotenv.js'),
+          [path.resolve(__dirname, './endpoints/seed')]: path.resolve(
+            __dirname,
+            './emptyModuleMock.js',
+          ),
+        },
+      },
+    }),
+  },
+  email: {
+    transportOptions: {
+      // For SMTP:
+      host: 'in-v3.mailjet.com',
+      port: '465',
+      auth: {
+        user: 'aa026f0fca4ebb1f6fee6bf392bfb1c7',
+        pass: 'c87f8ce27a8c0eeda5dc2388c5ffe6b5'
+      }
+    },
+    fromName: 'HelloConvo Marketing Agency',
+    fromAddress: 'agency@helloconvo.com'
+  },
+  editor: slateEditor({}),
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URI,
+  }),
+  serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
+  collections: [Pages, Posts, Projects, Media, Categories, Users, Comments],
+  globals: [Settings, Header, Footer],
+  typescript: {
+    outputFile: path.resolve(__dirname, 'payload-types.ts'),
+  },
+  graphQL: {
+    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
+  },
+  cors: [process.env.PAYLOAD_PUBLIC_SERVER_URL || ''].filter(Boolean),
+  csrf: [process.env.PAYLOAD_PUBLIC_SERVER_URL || ''].filter(Boolean),
+  endpoints: [
+    // The seed endpoint is used to populate the database with some example data
+    // You should delete this endpoint before deploying your site to production
+    {
+      path: '/seed',
+      method: 'get',
+      handler: seed,
+    },
+  ],
+  plugins: [
+    redirects({
+      collections: ['pages', 'posts'],
+    }),
+    nestedDocs({
+      collections: ['categories'],
+    }),
+    seo({
+      collections: ['pages', 'posts', 'projects'],
+      generateTitle,
+      uploadsCollection: 'media',
+    }),
+    payloadCloud(),
+    cloudStorage({
+      collections: { // Create an object for every upload collection, in this case it's only "media"
+        'media': {
+          adapter: storageAdapter,
+        },
+      },
+    }),
+  ],
+})
